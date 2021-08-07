ebf85695b6f0a0c9d2e77b0ecde248b7bba3694d
Ishan Srivastava <I24338@verisk.com>
2021-08-07 13:59:12 +0530

    MnotonicArray

---
+++ MonotonicArray.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Program {
+  public static boolean isMonotonic(int[] array) {
+    // Write your code here.
+		if(array.length<2)
+			return true;
+		int direction=array[1]-array[0];
+		
+		for(int i=2;i<array.length;i++)
+		{
+			if(direction==0)
+			{
+				direction=array[i]-array[i-1];
+				continue;
+			}
+			if(breakDirection(direction,array[i-1],array[i]))
+				return false;
+
+		}
+		return true;
+    
+  }
+	
+	public static boolean breakDirection(int direction,int previous,int current)
+	{
+		int difference=current-previous;
+		if(direction>0)
+			return difference<0;
+		return difference>0;
+	}
+}
