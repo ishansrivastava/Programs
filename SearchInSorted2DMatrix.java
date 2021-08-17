b598f89e6de1c5b8296682d4d34524d3ba1fff48
Ishan Srivastava <I24338@verisk.com>
2021-08-17 21:34:11 +0530

    SearchInSorted2DMatrix

--- SearchInSorted2DMatrix.java
+++ SearchInSorted2DMatrix.java
@@ -4,7 +4,7 @@ class Program {
   public static int[] searchInSortedMatrix(int[][] matrix, int target) {
     // Write your code here.
     int row=0;
-		int col=matrix[0].length-1;
+	int col=matrix[0].length-1;
 		
 		while(row<matrix.length && col>=0)
 		{
